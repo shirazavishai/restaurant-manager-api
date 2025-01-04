eef4702e3fa56d7241075ca30b2891ef17c214b5
shirazAvishai <shiraz.avishai@s.afeka.ac.il>
2025-01-04 17:05:51 +0200

    Retrieve the database connection string from the environment variable (injected by terraform)

--- restaurantsManage/app/db.py
+++ restaurantsManage/app/db.py
@@ -1,12 +1,14 @@
+import os
 import pyodbc
 from typing import Optional
 
-server = 'your-server-name.database.windows.net'
-database = 'your-database-name'
-username = 'your-username'
-password = 'your-password'
-driver = '{ODBC Driver 17 for SQL Server}'
+# Retrieve the database connection string from the environment variable (injected by terraform)
+connection_string = os.getenv('DATABASE_URL')
 
 def get_db_connection() -> pyodbc.Connection:
-    """Establish a connection to the Azure SQL Database."""
-    return pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}')
+    """Establish a connection to the Azure SQL Database using the connection string from app settings."""
+    if connection_string is None:
+        raise ValueError("DATABASE_URL environment variable is not set.")
+    
+    # Establish the database connection using the connection string
+    return pyodbc.connect(connection_string)
